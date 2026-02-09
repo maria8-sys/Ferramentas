eca1f0dff0e297711226a3a29ffa426fdc0ce914
maria8-sys <mahhduda008@gmail.com>
2026-02-09 14:09:01 -0300

    primeiro commit

---
+++ src/main/java/com/pbe/ferramentas/FerramentasApplication.java
@@ -0,0 +1,13 @@
+package com.pbe.ferramentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerramentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerramentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbe/ferramentas/FerramentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbe.ferramentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerramentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
